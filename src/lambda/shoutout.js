cc93dd73451d07881492086982d0ca1cd4ccfee1
Brian Yen-Bo Houng <bhoung@sjcmac70utjhd4.paloaltonetworks.local>
2019-01-29 11:52:12 -0800

    add firebase and edit shoutout

--- src/lambda/shoutout.js
+++ src/lambda/shoutout.js
@@ -1,11 +1,13 @@
 exports.handler = (event, context, callback) => {
   // Function code goes here
-  console.log("------------------------------------");
-  console.log(event);
-  console.log("------------------------------------");
   callback(null, {
     statusCode: 200,
     body: event.body,
-    data: event.body
+    text: "You have shouted out to: ",
+    attachments: [
+      {
+        text: event.body.text
+      }
+    ]
   });
 };
