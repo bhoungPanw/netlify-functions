cb411a591c0650f6c81743c31169c6a96956e2f4
Brian Yen-Bo Houng <bhoung@sjcmac70utjhd4.paloaltonetworks.local>
2019-01-29 12:01:13 -0800

    in body

--- src/lambda/shoutout.js
+++ src/lambda/shoutout.js
@@ -2,11 +2,13 @@ exports.handler = (event, context, callback) => {
   // Function code goes here
   callback(null, {
     statusCode: 200,
-    text: "You have shouted out to: ",
-    attachments: [
-      {
-        text: event.body.text
-      }
-    ]
+    body: {
+      text: "You have shouted out to: ",
+      attachments: [
+        {
+          text: "Partly cloudy today and tomorrow"
+        }
+      ]
+    }
   });
 };
