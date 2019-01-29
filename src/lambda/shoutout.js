0cbe6deea16530c846ee181679cc84ebd232108c
Brian Yen-Bo Houng <bhoung@sjcmac70utjhd4.paloaltonetworks.local>
2019-01-29 11:59:04 -0800

    move attachments out of body

--- src/lambda/shoutout.js
+++ src/lambda/shoutout.js
@@ -2,13 +2,11 @@ exports.handler = (event, context, callback) => {
   // Function code goes here
   callback(null, {
     statusCode: 200,
-    body: {
-      text: "You have shouted out to: ",
-      attachments: [
-        {
-          text: event.body.text
-        }
-      ]
-    }
+    text: "You have shouted out to: ",
+    attachments: [
+      {
+        text: event.body.text
+      }
+    ]
   });
 };
