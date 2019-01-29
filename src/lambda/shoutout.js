c2b25ffd1ad9694d50f85b3aa9d58800391559ae
Brian Yen-Bo Houng <bhoung@sjcmac70utjhd4.paloaltonetworks.local>
2019-01-29 11:56:34 -0800

    add attachments to body

--- src/lambda/shoutout.js
+++ src/lambda/shoutout.js
@@ -2,12 +2,13 @@ exports.handler = (event, context, callback) => {
   // Function code goes here
   callback(null, {
     statusCode: 200,
-    body: event.body,
-    text: "You have shouted out to: ",
-    attachments: [
-      {
-        text: event.body.text
-      }
-    ]
+    body: {
+      text: "You have shouted out to: ",
+      attachments: [
+        {
+          text: event.body.text
+        }
+      ]
+    }
   });
 };
