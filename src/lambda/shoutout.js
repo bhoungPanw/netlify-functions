23f05c5e1197b053715ac01c7ee4821bb288b4b7
Brian Yen-Bo Houng <bhoung@sjcmac70utjhd4.paloaltonetworks.local>
2019-01-29 12:22:11 -0800

    return response

--- src/lambda/shoutout.js
+++ src/lambda/shoutout.js
@@ -10,8 +10,10 @@ exports.handler = (event, context, callback) => {
     ]
   };
 
-  callback(null, {
-    statusCode: 200,
-    body: body
-  });
+  return { isBase64Encoded: True, statusCode: 200, headers: {}, body: body };
+
+  //   callback(null, {
+  //     statusCode: 200,
+  //     body: body
+  //   });
 };
