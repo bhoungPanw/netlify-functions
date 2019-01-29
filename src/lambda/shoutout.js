1e7e7b48efba4899dfde3f521c05ab09b8df8072
Brian Yen-Bo Houng <bhoung@sjcmac70utjhd4.paloaltonetworks.local>
2019-01-29 12:11:07 -0800

    stringify body

--- src/lambda/shoutout.js
+++ src/lambda/shoutout.js
@@ -1,14 +1,11 @@
 exports.handler = (event, context, callback) => {
   // Function code goes here
+  const body = JSON.stringify({
+    text: "You have shouted out to: "
+  });
+
   callback(null, {
     statusCode: 200,
-    body: {
-      text: "You have shouted out to: ",
-      attachments: [
-        {
-          text: "Partly cloudy today and tomorrow"
-        }
-      ]
-    }
+    body: body
   });
 };
